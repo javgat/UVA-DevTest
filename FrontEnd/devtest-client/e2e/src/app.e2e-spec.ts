ce55c5f2c0dd96a12089a102ad22b33203b65e3a
Javier Gatón Herguedas <javigaton@gmail.com>
2021-02-27 21:05:42 +0100

    comentarios e2e auto

--- FrontEnd/devtest-client/e2e/src/app.e2e-spec.ts
+++ FrontEnd/devtest-client/e2e/src/app.e2e-spec.ts
@@ -51,29 +51,32 @@ describe('workspace-project App', () => {
   });
   
   afterEach(async () => {
+    //Esto no va en firefox he leido, por eso lo comento, pero no lo quito, investigare mas adelante
     /*
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
     expect(logs).not.toContain(jasmine.objectContaining({
       level: logging.Level.SEVERE,
-    } as logging.Entry));*///Esto no va en firefox he leido
+    } as logging.Entry));*/
   });
 
-  it('should register and login as a new user', async() => {
+  it('should register user, log in as user, show errors when registering as existing user or logging in as unexisting user', async() => {
     let username = "username"
     let usernameWrong = "usernameWrong"
     let email = "username@email.com"
     let password = "password"
+    // Register a new user
     await signinPage.navigateTo();
     await signinPage.registerUser(username, email, password)
     expect(await browser.wait(EC.urlContains(loginPage.getUrl()), 5000).catch(() => {return false}))
       .toBeTruthy("Redireccion de signin exitoso a login no se realizo")
+    // Prompt an error when registering with an already registered user username/email data
     await signinPage.navigateTo()
     await signinPage.registerUser(username, email, password)
     expect(await help.isErrorMessage()).toBeTruthy("No hay mensaje de error al registrar usuario duplicado")
     await signinPage.registerUser(usernameWrong, email, password)
     expect(await help.isErrorMessage()).toBeTruthy("No hay mensaje de error al registrar email duplicado")
-
+    // Log in as an existing user with username/email
     await loginPage.navigateTo()
     await loginPage.loginUser(username, password)
     expect(await help.loginSuccess()).toBeTruthy("No se detecta feedback de login funcional con username")
@@ -82,10 +85,9 @@ describe('workspace-project App', () => {
     await loginPage.loginUser(email, password)
     expect(await help.loginSuccess()).toBeTruthy("No se detecta feedback de login funcional con email")
     await page.logout()
+    // Prompt error logging in as unexisting user 
     await loginPage.navigateTo()
     await loginPage.loginUser(usernameWrong, password)
     expect(await help.loginWrong()).toBeTruthy("No se detecta feedback de error de login con username inexistente")
-
-    
   })
 });
