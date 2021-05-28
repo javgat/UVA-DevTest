5aaef942a584181a51dd9685d075ba5eb9186ef4
Javier Gatón Herguedas <javigaton@gmail.com>
2021-05-28 15:07:56 +0200

    mensaje se autoborra si es success

--- FrontEnd/devtest-client/src/app/mensaje/mensaje.component.ts
+++ FrontEnd/devtest-client/src/app/mensaje/mensaje.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { Mensaje } from '../shared/app.model';
+import { Mensaje, Tipo } from '../shared/app.model';
 import { DataService } from '../shared/data.service';
 
 @Component({
@@ -12,11 +12,17 @@ export class MensajeComponent implements OnInit {
 
   private mensaje: Mensaje
   private messageSubscription: Subscription
-
+  private mostrar: boolean
+  private timerId?: number
   constructor(private data: DataService) {
+    this.mostrar = true
     this.mensaje = new Mensaje()
     this.messageSubscription = this.data.mensajeActual.subscribe(
-      valor => this.mensaje = valor
+      valor => {
+        this.mensaje = valor
+        this.mostrar = true
+        this.programarOcultacion()
+      }
     )
   }
 
@@ -29,5 +35,29 @@ export class MensajeComponent implements OnInit {
   
   getMensaje(): Mensaje {
     return this.mensaje
+  }
+
+  showMensaje(): boolean{
+    return this.getMensaje().mostrar && this.mostrar
+  }
+
+  ocultarMensaje(){
+    this.mostrar = false
+  }
+
+  ocultarMensajeOther(that: MensajeComponent){
+    that.ocultarMensaje()
+  }
+
+  programarOcultacion(){
+    if(this.timerId!=undefined){
+      clearTimeout(this.timerId);
+      this.timerId = undefined
+    }
+    console.log(this.getMensaje().trueType)
+    if(this.getMensaje().trueType == Tipo.SUCCESS){
+      console.log("en 3 secs adios")
+      this.timerId = setTimeout(this.ocultarMensajeOther, 2500, this)
+    }
   }
 }
