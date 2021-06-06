dd89010bfddb18144f7a8c1ddb99b1e2b252f2c1
Javier Gatón Herguedas <javigaton@gmail.com>
2021-06-06 12:08:37 +0200

    arreglado mensaje eterno de descargando tests en pendiente

--- FrontEnd/devtest-client/src/app/list-tests/lpt-pending/lpt-pending.component.ts
+++ FrontEnd/devtest-client/src/app/list-tests/lpt-pending/lpt-pending.component.ts
@@ -15,13 +15,19 @@ export class LptPendingComponent extends ListTestsComponent implements OnInit {
 
   id: string | undefined
   routeSub: Subscription
+  getIdFromSession: boolean
   constructor(session: SessionService, router: Router, data: DataService, userS: UserService, tS: TestService, tagS: TagService, private route: ActivatedRoute) {
     super(session, router, data, userS, tS, tagS)
+    this.getIdFromSession=false
     this.id=""
     this.includeNonEdit = true
     this.arePublished = true
     this.routeSub = this.route.params.subscribe(params => {
       this.id = params['username']
+      if(this.id==undefined){
+        this.getIdFromSession=true
+        this.id=this.getSessionUser().getUsername()
+      }
       this.borrarMensaje()
       this.getTestsFilters()
     });
@@ -35,13 +41,24 @@ export class LptPendingComponent extends ListTestsComponent implements OnInit {
     this.routeSub.unsubscribe()
   }
 
+  doHasUserAction(){
+    if(this.getIdFromSession){
+      this.id=this.getSessionUser().getUsername()
+    }
+    this.getTestsFilters()
+    super.doHasUserAction()
+  }
+
   getTestsInclude(primera: boolean) {
     this.getTestsEdit(true)
   }
 
   // EN published tests testEdit es el testPublished
   getTestsEdit(primera: boolean) {
-    if(this.id==undefined) return
+    if(this.id==undefined || this.id==""){
+      this.borrarMensaje()
+      return
+    } 
     this.userS.getPendingTestsFromUser(this.id, this.searchTags, this.likeTitle, this.orderBy, this.limit, this.offset).subscribe(
       resp => this.saveTests(resp),
       err => this.handleErrRelog(err, "obtener tests pendientes de un usuario", primera, this.getTestsEdit, this)
